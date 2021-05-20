620a8b2130b8b90eb3827157b08d2fb28e49f0c4
Nischal <31039585+nischal-km@users.noreply.github.com>
2021-05-21 00:08:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,15 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-from transformers import pipeline
+st.title('Timers')
+st.subheader('An app which helps you to know at what time you should post')
 
-nlp = pipeline("sentiment-analysis")
-
-st.text(nlp("I hate you"))
-st.text(nlp("I love you"))
-
-st.title('Writer buddy')
-st.subheader('An app which helps you to write better')
-
-st.text_area('Write your text')
 st.button('Suggest')
