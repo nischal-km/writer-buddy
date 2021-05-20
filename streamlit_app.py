ede40c986e432befc241965e3f48834234d9177f
Nischal <31039585+nischal-km@users.noreply.github.com>
2021-05-21 00:16:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Timers')
-st.subheader('An app which helps you to know at what time you should post')
+st.title('Writer buddy')
+st.subheader('An app which helps you to write better or some inspiration')
 
 st.button('Suggest')
