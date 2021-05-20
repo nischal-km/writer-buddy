62a906646ffd63be68ab3318465cbeef57e4473d
Nischal <31039585+nischal-km@users.noreply.github.com>
2021-05-21 00:44:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,23 +7,23 @@ from aitextgen import aitextgen
 st.title('Writer buddy')
 st.subheader('An app which helps you to write better or some inspiration')
 st.text('This app is just for educational purpose')
-st.button('Suggest')
 
 # instantiate the model / download
 ai = aitextgen()
 
 # create a prompt text for the text generation 
 #prompt_text = "Python is awesome"
-prompt_text = st.text_input(label = "Enter your prompt text...",
-            value = "Computer is beautiful")
+prompt_text = st.text_input(label = "Enter your story",
+            value = "One upon a king")
 
-with st.spinner("AI is at Work........"):
+with st.spinner("Generating suggestions for you"):
     # text generation
     gpt_text = ai.generate_one(prompt=prompt_text,
-            max_length = 100 )
-st.success("AI Successfully generated the below text ")
+            max_length = 1000)
+            
+st.success("Suggestions has been created")
 st.balloons()
-# print ai generated text
-print(gpt_text)
-
 st.text(gpt_text)
+
+
+
