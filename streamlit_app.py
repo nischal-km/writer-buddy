0df02c37f38d5d1a7f2016b70c733552f6699152
Nischal <31039585+nischal-km@users.noreply.github.com>
2021-05-21 00:07:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,21 +2,15 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+from transformers import pipeline
+
+nlp = pipeline("sentiment-analysis")
+
+st.text(nlp("I hate you"))
+st.text(nlp("I love you"))
+
 st.title('Writer buddy')
 st.subheader('An app which helps you to write better')
 
 st.text_area('Write your text')
 st.button('Suggest')
-
-tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
-
-# add the EOS token as PAD token to avoid warnings
-model = TFGPT2LMHeadModel.from_pretrained("gpt2", pad_token_id=tokenizer.eos_token_id)
-
-# encode context the generation is conditioned on
-input_ids = tokenizer.encode('I enjoy walking with my cute dog', return_tensors='tf')
-
-# generate text until the output length (which includes the context length) reaches 50
-greedy_output = model.generate(input_ids, max_length=50)
-ot = tokenizer.decode(greedy_output[0], skip_special_tokens=True)
-st.text(ot)
