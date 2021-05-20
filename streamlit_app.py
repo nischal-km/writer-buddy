732016dc726a1abf61357134f40852e3e704049f
Nischal <31039585+nischal-km@users.noreply.github.com>
2021-05-20 23:59:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,9 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from transformers import GPT2Tokenizer, GPT2LMHeadModel
+import tensorflow as tf
+from transformers import TFGPT2LMHeadModel, GPT2Tokenizer
 
-tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
-model = GPT2LMHeadModel.from_pretrained("gpt2", pad_token_id=tokenizer.eos_token_id)
-
-inputs = tokenizer("I am a full stack developer and also interested in machine learning ", return_tensors="pt")
-generation_output = model.generate(**inputs, max_length=50)
-ot = tokenizer.decode(generation_output[0], skip_special_tokens=True)
 
 st.title('Writer buddy')
 st.subheader('An app which helps you to write better')
@@ -17,3 +12,19 @@ st.text_area('Write your text')
 st.button('Suggest')
 
 st.text(ot)
+
+
+
+tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+
+# add the EOS token as PAD token to avoid warnings
+model = TFGPT2LMHeadModel.from_pretrained("gpt2", pad_token_id=tokenizer.eos_token_id)
+
+# encode context the generation is conditioned on
+input_ids = tokenizer.encode('I enjoy walking with my cute dog', return_tensors='tf')
+
+# generate text until the output length (which includes the context length) reaches 50
+greedy_output = model.generate(input_ids, max_length=50)
+
+print("Output:\n" + 100 * '-')
+print(tokenizer.decode(greedy_output[0], skip_special_tokens=True))
