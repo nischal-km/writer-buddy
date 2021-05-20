fc14838a7e5d52cd5bb443ad5e7e645dc7093704
Nischal <31039585+nischal-km@users.noreply.github.com>
2021-05-21 00:17:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,18 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+from aitextgen import aitextgen
+
+# Without any parameters, aitextgen() will download, cache, and load the 124M GPT-2 "small" model
+ai = aitextgen()
+
+ai.generate()
+ai.generate(n=3, max_length=100)
+output = ai.generate(n=3, prompt="I believe in unicorns because", max_length=100)
+
 st.title('Writer buddy')
 st.subheader('An app which helps you to write better or some inspiration')
 
 st.button('Suggest')
+
+st.text(output)
